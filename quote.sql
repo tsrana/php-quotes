8f976f19daa67f4816310a755bdc111234aec03c
Tejendra Rana <tejendra.rana@in.ibm.com>
2021-05-29 23:49:35 +0530

    4

--- quote.sql
+++ quote.sql
@@ -1,5 +1,5 @@
 CREATE TABLE quote (id int, msg varchar(255));
-INSERT INTO quote VALUES (1, 'Ts Rana');
-INSERT INTO quote VALUES (2, 'Deepika');
-INSERT INTO quote VALUES (3, 'Sameeksha');
-INSERT INTO quote VALUES (4, 'Vedanshi');
+INSERT INTO quote VALUES (1, 'Love For All, Hatred For None');
+INSERT INTO quote VALUES (2, 'Change the world by being yourself');
+INSERT INTO quote VALUES (3, 'Every moment is a fresh beginning');
+INSERT INTO quote VALUES (4, 'Die with memories, not dreams');
--- work.bat
+++ work.bat
@@ -1,1 +1,3 @@
 oc cp C:\Users\TEJENDRARANA\Desktop\Rana\Study\GIT_Clones\php-quotes $(oc get pods --no-headers -o custom-columns=POD:.metadata.name -l app=quotesdb):/tmp
+
+mysql -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE < /tmp/quote.sql 
